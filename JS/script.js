e356d3c3683aaa69b805e9cd52a878c0a0fcf6ab
Christian Fuchs <58251727+MrFox09@users.noreply.github.com>
2020-04-02 11:11:55 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
-/*jshint esversion: 6 */
-const galleryDiv = document.getElementById('gallery');
+/*jshint esversion: 8 */
+
+
 
 /***
 
@@ -9,12 +10,20 @@ logs an error when something goes wron
 
 ***/
 
-const fetchUser = (url) => {
+const fetchUser = async (url) => {
 
-  return fetch(url)
-    .then(checkStatus)
-    .then(response => response.json())
-    .catch(error => console.log('Something goes wrong',error));
+  const response = await fetch(url);
+  if (checkStatus(response)) {
+
+    const data = await response.json();
+    const results = await data.results;
+
+
+    return results;
+
+  }
+
+
 };
 
 
@@ -43,6 +52,7 @@ Builds the HTML to show every user in his own card.
  ***/
 
 const galleryHTML = (user) => {
+  const galleryDiv = document.getElementById('gallery');
 
 
   const parentCardDiv = document.createElement('div');
@@ -56,7 +66,7 @@ const galleryHTML = (user) => {
   const img = document.createElement('img');
 
   img.setAttribute('class','card-img');
-  img.setAttribute('src',`${user.picture.medium}`);
+  img.setAttribute('src',`${user.picture.large}`);
   img.setAttribute('alt','profile picture');
 
   imgDiv.appendChild(img);
@@ -72,6 +82,44 @@ const galleryHTML = (user) => {
   parentCardDiv.appendChild(infoDiv);
   galleryDiv.appendChild(parentCardDiv);
 
+  parentCardDiv.addEventListener('click', () => {
+
+    modalHTML(user);
+
+  });
+
+
+
+
+};
+
+
+const modalHTML = (user) => {
+
+      const modalDiv = document.createElement('div');
+        modalDiv.setAttribute('class', 'modal-container');
+
+        // convert birthdate from JSON to JS
+        const birthdayJSON = user.dob.date;
+        const birthdayJS = new Date(birthdayJSON);
+
+        modalDiv.innerHTML = `<div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src= ${user.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.city}</p>
+                <hr>
+                <p class="modal-text">${user.phone}</p>
+                <p class="modal-text">${user.location.street}, ${user.location.state} ${user.location.postcode}</p>
+                <p class="modal-text">Birthday: ${birthdayJS}</p>
+            </div>`;
+
+      document.body.appendChild(modalDiv);
+
+
+
 };
 
 /***
@@ -81,11 +129,16 @@ call galleryHTML function for every user
 
  ***/
 
-fetchUser('https://randomuser.me/api/?results=12')
-  .then(data => {
-    const userAll = data.results;
 
-    userAll.forEach(user => {
+
+fetchUser('https://randomuser.me/api/?results=12')
+
+  .then((data) => {
+
+    data.forEach(user => {
       galleryHTML(user);
+
     });
+
+
   });
